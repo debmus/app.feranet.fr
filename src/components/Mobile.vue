b91e645ed2df0345287c2eb2bc706b56f3bf8346
debmus <contact@feranet.fr>
2020-12-27 20:21:34 +0100

    fix: update

--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -3,7 +3,6 @@
     <v-col class="text-center" cols="12">
       <v-bottom-sheet v-model="sheet" hide-overlay>
         <template v-slot:activator="{ on, attrs }">
-          <!-- <v-btn color="purple" dark v-bind="attrs" v-on="on"> apps </v-btn> -->
           <v-icon x-large color="primary" v-bind="attrs" v-on="on">apps</v-icon>
         </template>
         <div @click="sheet = false">
