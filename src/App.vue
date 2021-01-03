de7d83f291b3fa3006e3a5da388e5c8cd4426a35
debmus <contact@feranet.fr>
2021-01-03 14:37:12 +0100

    add: button go top

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
       <router-view></router-view>
     </v-main>
 
+    <go-top :size="50" bg-color="#2d88ed"></go-top>
+
     <Footer class="hidden-sm-and-down" />
     <Mobile class="hidden-md-and-up" />
   </v-app>
@@ -15,6 +17,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Mobile from "./components/Mobile";
+import GoTop from "@inotom/vue-go-top";
 
 export default {
   name: "App",
@@ -22,6 +25,7 @@ export default {
     Navbar,
     Footer,
     Mobile,
+    GoTop,
   },
   data: () => ({
     //
